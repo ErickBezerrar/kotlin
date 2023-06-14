bdcdedc49d7a5c00140ad337a86ae8bfc41e1976
ErickBezerrar <erickbrtrindade@gmail.com>
2023-06-14 09:56:06 -0300

    kotlin -> Declaração de variáveis

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,27 @@ fun main(args: Array<String>) {
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
     println("Yes!")
+
+    var name: String = "Erick"
+    var idade: Int = 18
+    println(name)
+    println(idade)
+
+    println("Double MAX ${Double. MAX_VALUE} - MIN ${Double.MIN_VALUE}")
+    println("Float MAX ${Float. MAX_VALUE} - MIN ${Float.MIN_VALUE}")
+    println("Long MAX ${Long. MAX_VALUE} - MIN ${Long.MIN_VALUE}")
+    println("Int MAX ${Int. MAX_VALUE} - MIN ${Int.MIN_VALUE}")
+    println("Short MAX ${Short.MAX_VALUE} - WIN ${Short.MIN_VALUE}")
+    println("Byte MAX ${Byte.MAX_VALUE} - WIN ${Byte.MIN_VALUE}")
+
+    var c: Char = 'a'
+    var S: String = "hsdffsddf sdf segsdfg sdfg sdg sd rgiybser ibsdflhb dsflgib dfg bdsgflisdbgflsdhb"
+    var b: Boolean = true
+    var double: Double = 10.0
+    var float: Float = 10f
+    var long: Long = 10L
+    var short: Short = 10
+    var byte: Byte = 10
+    var int: Int = 40
+
 }
